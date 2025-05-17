b68eb7d9530f89e6f3701a52457a1d7f0c043226
DevPedroAugusto-BR <pedroaugusto.mendes035@gmail.com>
2025-05-17 19:33:48 -0300

    Add the new files of the course FastAPI

--- src/learning_fastapi/app.py
+++ src/learning_fastapi/app.py
@@ -1,8 +1,11 @@
+from http import HTTPStatus
 from fastapi import FastAPI
+
+from learning_fastapi.schemas import Message
 
 app = FastAPI()
 
 
-@app.get("/Batatinha")
+@app.get("/Batatinha", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Hello World"}
